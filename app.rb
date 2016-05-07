17538cc17430ad4375c9c9a80f980e92ec7201d3
Qiu Juntao <juntao.qiu@gmail.com>
2016-05-07 23:35:35 +1000

    code clean up

--- app.rb
+++ app.rb
@@ -2,18 +2,20 @@ require 'sinatra'
 require 'uri'
 
 set :return_url, ''
+set :bind, '0.0.0.0'
+set :w3_id, 'wwx363344'
 
 get '/sso' do
-    settings.return_url = params[:return]+"?w3=wwx363344"
+    settings.return_url = params[:return]+"?w3=#{settings.w3_id}"
     send_file 'public/index.html'
 end
 
 post '/login' do
 	uri = URI.parse(settings.return_url == '' ? 'localhost': settings.return_url)
 	response.set_cookie("w3", {
-		:domain => uri.host,
+		:domain => ".#{uri.host}",
 	    :expires => Time.now + 2400,
-	    :value => "wwx363344",
+	    :value => "#{settings.w3_id}",
 	    :path => '/'
   	})
 	redirect settings.return_url, 302

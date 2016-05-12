ef04e87e486f562b304df47c65191e06ca9b757f
Qiu Juntao <juntao.qiu@gmail.com>
2016-05-12 22:07:10 +1000

    add credential in form

--- app.rb
+++ app.rb
@@ -3,19 +3,19 @@ require 'uri'
 
 set :return_url, ''
 set :bind, '0.0.0.0'
-set :w3_id, 'wwx363344'
 
 get '/sso' do
-    settings.return_url = params[:return]+"?w3=#{settings.w3_id}"
+    settings.return_url = params[:return]
     send_file 'public/index.html'
 end
 
 post '/login' do
-	uri = URI.parse(settings.return_url == '' ? 'localhost': settings.return_url)
+	credential = params[:credential]
+	uri = URI.parse(settings.return_url)
 	response.set_cookie("w3", {
 		:domain => ".#{uri.host}",
 	    :expires => Time.now + 2400,
-	    :value => "#{settings.w3_id}",
+	    :value => "#{credential['name']}",
 	    :path => '/'
   	})
 	redirect settings.return_url, 302
